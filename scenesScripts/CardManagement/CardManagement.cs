c678e03f2e09c258294a4ea27531a754c7248ece
yquanjun <yquanjun@gmail.com>
2026-03-26 03:47:11 +0800

    Added clickable buttons for cards in collection

--- scenesScripts/CardManagement/CardManagement.cs
+++ scenesScripts/CardManagement/CardManagement.cs
@@ -2,10 +2,11 @@ using Godot;
 using System;
 using System.Collections.Generic; 
 using System.Threading.Tasks;
+using System.Linq;
 	
 public partial class CardManagement : Control
 {
-	public PackedScene CardScene = GD.Load<PackedScene>("res://scenes/gameComponents/Card.tscn");
+	private PackedScene CardScene = GD.Load<PackedScene>("res://scenes/gameComponents/Card.tscn");
 	private List<FlowContainer> Decks = new List<FlowContainer>();
 	
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,12 +17,12 @@ public partial class CardManagement : Control
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		generate_collection();
-		generate_decks();
+		GenerateCollection();
+		GenerateDecks();
 	}
 	
 	// Function to generate card collection
-	public void generate_collection()
+	public void GenerateCollection()
 	{
 		// insert cards into collection
 		var CollectionContainer = GetNode<FlowContainer>("MainCardContainer/CollectionContainer/CollectionColorContainer/ScrollContainer/CollectionContainer");
@@ -29,16 +30,41 @@ public partial class CardManagement : Control
 		// Generate 13 items
 		for (int i = 0; i < 13; i++)
 		{
+			// to pass the values over to any functions (passing i will always pass max)
+			int index = i;
+			
 			var CardContainer = new VBoxContainer();
 				
 			// Create control that gives the card its size
 			var CardControl = new Control();
-			CardControl.CustomMinimumSize = new Vector2(200, 230);
+			CardControl.Name = "CardControl";
+			CardControl.CustomMinimumSize = new Vector2(190, 200);
 			
 			// Create the card itself
 			var Card = CardScene.Instantiate<Node2D>();
-			Card.Position = new Vector2(100, 115);
+			Card.Position = new Vector2(95, 100);
 			CardControl.AddChild(Card);
+			
+			// Add card button overlay
+			var CardButtonOverlay = new Button();
+			CardButtonOverlay.CustomMinimumSize = new Vector2(170, 200);
+			CardButtonOverlay.Position = new Vector2(10, 0);
+			CardButtonOverlay.ZIndex = 2;
+			CardButtonOverlay.MouseDefaultCursorShape = Control.CursorShape.PointingHand;
+			
+			// Make all states fully transparent
+			var Transparent = new StyleBoxFlat();
+			Transparent.BgColor = new Color(0, 0, 0, 0); // RGBA, alpha = 0 (transparent)
+
+			CardButtonOverlay.AddThemeStyleboxOverride("normal", Transparent);
+			CardButtonOverlay.AddThemeStyleboxOverride("hover", Transparent);
+			CardButtonOverlay.AddThemeStyleboxOverride("pressed", Transparent);
+			CardButtonOverlay.AddThemeStyleboxOverride("focus", Transparent);
+			CardButtonOverlay.AddThemeStyleboxOverride("disabled", Transparent);
+
+			// Connect pressed to a function
+			CardButtonOverlay.Pressed += () => OnCollectionCardPressed(index);
+			CardControl.AddChild(CardButtonOverlay);
 			
 			// Add the card into the container
 			CardContainer.AddChild(CardControl);
@@ -54,11 +80,127 @@ public partial class CardManagement : Control
 			
 			// Add the card into the main container
 			CollectionContainer.AddChild(CardContainer);
+			
 		}
 	}
 	
+	private void OnCollectionCardPressed(int index){
+		var CollectionContainer = GetNode<FlowContainer>("MainCardContainer/CollectionContainer/CollectionColorContainer/ScrollContainer/CollectionContainer");
+		
+		VBoxContainer CardContainer = CollectionContainer.GetChild(index) as VBoxContainer;
+		
+		Control CardControl = CardContainer.GetNode<Control>("CardControl");
+		Vector2 pos = CardContainer.Position;
+		
+		// Check if card button exists
+		int ButtonIndex = CheckCardButton(pos.X, pos.Y);
+		
+		// Button does not exist yet
+		if(ButtonIndex == -1){
+			// Create button
+			CreateCardButton(pos.X, pos.Y, index);
+			GD.Print(pos.Y);
+			RefreshScrollbar();
+		}else{
+			// Else deletes it
+			DeleteCardButton(ButtonIndex);
+		}
+	}
+	
+	private void RefreshScrollbar(){
+		GD.Print("TotalHeightCA");
+		ScrollContainer Scroll = GetNode<ScrollContainer>("MainCardContainer/CollectionContainer/CollectionColorContainer/ScrollContainer");
+		HFlowContainer Content = Scroll.GetNode<HFlowContainer>("CollectionContainer");
+
+		// Calculate total Content height
+		GD.Print("TotalHeightA");
+		float TotalHeight = 0;
+		foreach(Control Child in Content.GetChildren().OfType<Control>())
+		{
+			Vector2 pos = Child.Position;
+			if(TotalHeight < pos.Y){
+				TotalHeight = pos.Y;
+			}
+		}
+		GD.Print(TotalHeight);
+
+		float DesiredHeight = TotalHeight + 300;
+		GD.Print(DesiredHeight);
+		Content.CustomMinimumSize = new Vector2(Scroll.Size.X, DesiredHeight);
+		Scroll.QueueSort();  // Refresh layout
+	}
+	
+	// Function to check if card button set exists, return the index of the element child
+	private int CheckCardButton(float X, float Y){
+		var ButtonContainer = GetNode<Control>("MainCardContainer/CollectionContainer/CollectionColorContainer/ScrollContainer/ButtonContainer");
+		
+		for (int i = 0; i < ButtonContainer.GetChildCount(); i++){
+			Control ButtonSet = ButtonContainer.GetChild(i) as Control;
+			// use position to find the first button
+			if(ButtonSet.Position == new Vector2(X + 10, Y + 200)){
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	private void DeleteCardButton(int index){
+		var ButtonContainer = GetNode<Control>("MainCardContainer/CollectionContainer/CollectionColorContainer/ScrollContainer/ButtonContainer");
+		
+		Control ButtonSet = ButtonContainer.GetChild(index) as Control;
+		ButtonSet.QueueFree();
+	}
+	
+	// Function to create the 2 buttons
+	private void CreateCardButton(float X, float Y, int index){
+		var ButtonSet = new Control();
+		ButtonSet.Position = new Vector2(X + 10, Y + 200);
+		
+		// Add the 2 button (more info & add to deck)
+		var MoreInfoButton = new Button();
+		var AddToDeckButton = new Button();
+		
+		MoreInfoButton.CustomMinimumSize = new Vector2(170, 40);
+		MoreInfoButton.Position = new Vector2(0, 0);
+		MoreInfoButton.Text = "More Info";
+		MoreInfoButton.ZIndex = 3;
+		MoreInfoButton.AddThemeFontSizeOverride("font_size", 20);
+		MoreInfoButton.AddThemeColorOverride("font_color", new Color(255, 255, 255));
+		MoreInfoButton.MouseDefaultCursorShape = Control.CursorShape.PointingHand;
+			
+		AddToDeckButton.CustomMinimumSize = new Vector2(170, 40);
+		AddToDeckButton.Position = new Vector2(0, 40);
+		AddToDeckButton.Text = "Add";
+		AddToDeckButton.ZIndex = 3;
+		AddToDeckButton.AddThemeFontSizeOverride("font_size", 20);
+		AddToDeckButton.AddThemeColorOverride("font_color", new Color(255, 255, 255));
+		AddToDeckButton.MouseDefaultCursorShape = Control.CursorShape.PointingHand;
+		
+		StyleBox button = GD.Load<StyleBox>("res://styles/button.tres");
+		StyleBox ButtonHover = GD.Load<StyleBox>("res://styles/button.tres");
+		StyleBox ButtonPressed = GD.Load<StyleBox>("res://styles/button.tres");
+
+		MoreInfoButton.AddThemeStyleboxOverride("normal", button);
+		MoreInfoButton.AddThemeStyleboxOverride("hover", ButtonHover);
+		MoreInfoButton.AddThemeStyleboxOverride("pressed", ButtonPressed);
+			
+		AddToDeckButton.AddThemeStyleboxOverride("normal", button);
+		AddToDeckButton.AddThemeStyleboxOverride("hover", ButtonHover);
+		AddToDeckButton.AddThemeStyleboxOverride("pressed", ButtonPressed);
+			
+		// Connect pressed to a function
+		MoreInfoButton.Pressed += () => OpenCardPopup(index);
+		AddToDeckButton.Pressed += () => OpenCardPopup(index);
+		
+		ButtonSet.AddChild(MoreInfoButton);
+		ButtonSet.AddChild(AddToDeckButton);
+	
+		var ButtonContainer = GetNode<Control>("MainCardContainer/CollectionContainer/CollectionColorContainer/ScrollContainer/ButtonContainer");
+		ButtonContainer.AddChild(ButtonSet);
+	}
+	
 	// Function to generate Decks
-	public async Task generate_decks()
+	private async Task GenerateDecks()
 	{
 		// Generate 3 decks
 		for (int j = 0; j < 3; j++)
@@ -123,23 +265,31 @@ public partial class CardManagement : Control
 		}
 	}
 
-	public void _on_back_button_pressed()
+	public void _OnBackButtonPressed()
 	{
 		var GameStateManager = GetNode<GameStateManager>("/root/GameStateManager");
 		GameStateManager.ChangeGameState(GameState.HOMESCREEN);
 	}
 	
-	public void _on_card_popup_background_pressed()
+	public void _OnCardPopupBackgroundPressed()
 	{
-		close_card_popup();
+		CloseCardPopup();
 	}	
 	
-	public void _on_close_card_button_pressed()
+	public void _OnCloseCardButtonPressed()
 	{
-		close_card_popup();
+		CloseCardPopup();
 	}	
 	
-	public void close_card_popup()
+	private void OpenCardPopup(int index)
+	{
+		Control CardPopupContainerNode = GetNode<Control>("CardPopupContainer");
+		
+		// Turn it OFF (make visible false)
+		CardPopupContainerNode.Visible = true;
+	}	
+	
+	public void CloseCardPopup()
 	{
 		Control CardPopupContainerNode = GetNode<Control>("CardPopupContainer");
 		
--- scenesScripts/home/Home.cs
+++ scenesScripts/home/Home.cs
@@ -6,111 +6,111 @@ using FSDClient.autoLoad;
 
 public partial class Home : Control
 {
-    private volatile bool _searching = false;
-    private PlayerStateManager CurrentPlayer { get; set; }
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        try
-        {
-            CurrentPlayer = PlayerStateManager.Instance;
-            GD.Print(CurrentPlayer.ToString());
-            if (CurrentPlayer.PlayerData == null)
-            {
-                GD.Print("The PlayerData is empty");
-                var GameStateManager = GetNode<GameStateManager>("/root/GameStateManager");
-                GameStateManager.ChangeGameState(GameState.LOGIN);
-            }
+	private volatile bool _searching = false;
+	private PlayerStateManager CurrentPlayer { get; set; }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		try
+		{
+			CurrentPlayer = PlayerStateManager.Instance;
+			GD.Print(CurrentPlayer.ToString());
+			if (CurrentPlayer.PlayerData == null)
+			{
+				GD.Print("The PlayerData is empty");
+				var GameStateManager = GetNode<GameStateManager>("/root/GameStateManager");
+				GameStateManager.ChangeGameState(GameState.LOGIN);
+			}
 
-        }
-        catch (Exception e)
-        {
-            GD.PrintErr("Whoops ", e);
-        }
-    }
+		}
+		catch (Exception e)
+		{
+			GD.PrintErr("Whoops ", e);
+		}
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
 
-    //	Press Battle button
-    public async void _on_battle_button_pressed()
-    {
+	//	Press Battle button
+	public async void _on_battle_button_pressed()
+	{
 
-        Control loadingNode = GetNode<ColorRect>("Loading");
+		Control loadingNode = GetNode<ColorRect>("Loading");
 
-        // start animation
-        AnimatedSprite2D LoadingSprite = GetNode<AnimatedSprite2D>("Loading/LoadingAnimation");
-        LoadingSprite.Play("loading");  // Replace "default" with your animation name
+		// start animation
+		AnimatedSprite2D LoadingSprite = GetNode<AnimatedSprite2D>("Loading/LoadingAnimation");
+		LoadingSprite.Play("loading");  // Replace "default" with your animation name
 
-        // Turn it ON (make visible)
-        loadingNode.Visible = true;
-        _searching = true;
+		// Turn it ON (make visible)
+		loadingNode.Visible = true;
+		_searching = true;
 
-        // find game stuff
-        while (_searching)
-        {
-            // 1 SECOND TIMEOUT - Godot way
-            await ToSignal(GetTree().CreateTimer(1.0f), "timeout");
+		// find game stuff
+		while (_searching)
+		{
+			// 1 SECOND TIMEOUT - Godot way
+			await ToSignal(GetTree().CreateTimer(1.0f), "timeout");
 
-            // if it did not get canceled
-            if (_searching)
-            {
-                // found match
-                _searching = false;
-                var GameStateManager = GetNode<GameStateManager>("/root/GameStateManager");
-                GameStateManager.ChangeGameState(GameState.INGAMEMODE);
-            }
-        }
-    }
+			// if it did not get canceled
+			if (_searching)
+			{
+				// found match
+				_searching = false;
+				var GameStateManager = GetNode<GameStateManager>("/root/GameStateManager");
+				GameStateManager.ChangeGameState(GameState.INGAMEMODE);
+			}
+		}
+	}
 
-    // Press card button
-    public void _on_card_button_pressed()
-    {
-        var GameStateManager = GetNode<GameStateManager>("/root/GameStateManager");
-        GameStateManager.ChangeGameState(GameState.CARDSCREEN);
-    }
+	// Press card button
+	public void _on_card_button_pressed()
+	{
+		var GameStateManager = GetNode<GameStateManager>("/root/GameStateManager");
+		GameStateManager.ChangeGameState(GameState.CARDSCREEN);
+	}
 
-    // Press cancel button when finding match
-    public void _on_cancel_button_pressed()
-    {
-        _searching = false;
-        Control loadingNode = GetNode<ColorRect>("Loading");
+	// Press cancel button when finding match
+	public void _on_cancel_button_pressed()
+	{
+		_searching = false;
+		Control loadingNode = GetNode<ColorRect>("Loading");
 
-        // stop animation
-        AnimatedSprite2D LoadingSprite = GetNode<AnimatedSprite2D>("Loading/LoadingAnimation");
-        LoadingSprite.Stop();  // Replace "default" with your animation name
+		// stop animation
+		AnimatedSprite2D LoadingSprite = GetNode<AnimatedSprite2D>("Loading/LoadingAnimation");
+		LoadingSprite.Stop();  // Replace "default" with your animation name
 
-        // Turn it OFF (make visible false)
-        loadingNode.Visible = false;
-    }
+		// Turn it OFF (make visible false)
+		loadingNode.Visible = false;
+	}
 
-    // Press friend button (to show friend list)
-    public void _on_friend_button_pressed()
-    {
-        Control FriendPopupContainerNode = GetNode<Control>("FriendPopupContainer");
+	// Press friend button (to show friend list)
+	public void _on_friend_button_pressed()
+	{
+		Control FriendPopupContainerNode = GetNode<Control>("FriendPopupContainer");
 
-        // Turn it ON (make visible)
-        FriendPopupContainerNode.Visible = true;
-    }
+		// Turn it ON (make visible)
+		FriendPopupContainerNode.Visible = true;
+	}
 
-    public void _on_friend_popup_background_pressed()
-    {
-        close_friend_popup();
-    }
+	public void _on_friend_popup_background_pressed()
+	{
+		close_friend_popup();
+	}
 
-    public void _on_close_friend_button_pressed()
-    {
-        close_friend_popup();
-    }
+	public void _on_close_friend_button_pressed()
+	{
+		close_friend_popup();
+	}
 
-    public void close_friend_popup()
-    {
-        Control FriendPopupContainerNode = GetNode<Control>("FriendPopupContainer");
+	public void close_friend_popup()
+	{
+		Control FriendPopupContainerNode = GetNode<Control>("FriendPopupContainer");
 
-        // Turn it OFF (make visible false)
-        FriendPopupContainerNode.Visible = false;
-    }
+		// Turn it OFF (make visible false)
+		FriendPopupContainerNode.Visible = false;
+	}
 
 }
