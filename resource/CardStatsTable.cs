672e946c56381b92a12b3619c44742e2ec33e2fd
yquanjun <yquanjun@gmail.com>
2026-04-02 03:27:57 +0800

    updated card resource script and all resources

--- resource/CardStatsTable.cs
+++ resource/CardStatsTable.cs
@@ -3,9 +3,9 @@ namespace FSDClient.resource;
 using Godot;
 public partial class CardStatsTable : Resource
 {
-    public Godot.Collections.Dictionary<int, CardStats> cardInfo = new();
-    
-    public CardStatsTable () {
-        // Load from some stupid csv or something idk 
-    }
+	public Godot.Collections.Dictionary<int, CardStats> cardInfo = new();
+	
+	public CardStatsTable () {
+		// Load from some stupid csv or something idk 
+	}
 }
