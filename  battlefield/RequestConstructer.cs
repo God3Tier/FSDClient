a78d30c788d4b5bad85968710bb364f8a2001039
Yam Kar Lok <94188697+kKar1503@users.noreply.github.com>
2026-04-06 19:53:12 +0800

    fix RequestConstructor params double-encoding and action casing

---  battlefield/RequestConstructer.cs
+++  battlefield/RequestConstructer.cs
@@ -18,7 +18,7 @@ public class RequestConstructor
 	[JsonPropertyName("action")]
 	private string Action { get; set; }
 	[JsonPropertyName("params")]
-	private string Params { get; set; }
+	private JsonElement Params { get; set; }
 	[JsonPropertyName("state_hash_after")]
 	private ulong StateHashAfter { get; set; }
 	[JsonPropertyName("sequence_number")]
@@ -27,8 +27,11 @@ public class RequestConstructor
 
 	public string GenerateRequest(RequestAction req, string parameters, PlayerState playerstate)
 	{
-		Params = parameters;
-		Action = req.ToString().ToLower();
+		if (string.IsNullOrEmpty(parameters))
+			Params = JsonSerializer.Deserialize<JsonElement>("{}");
+		else
+			Params = JsonSerializer.Deserialize<JsonElement>(parameters);
+		Action = req.ToString();
 		StateHashAfter = playerstate.HashPlayerView();
 		SequenceNumber = playerstate.SequenceNumber;
 		return JsonSerializer.Serialize(this);
